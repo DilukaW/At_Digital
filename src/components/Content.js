4c6770b4e7e340b3fee2338e3b005aa6a13f131e
Diluka <dt.wj85@gmail.com>
2024-04-07 08:53:06 +0530

    error fix

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ const Content = () => {
     return (
         <div>
             <div className="container d-md-flex d-block align-items-center justify-content-center">
-                <img src={img2} className='img-fluid text-center w-100'></img>
+                <img src={img2} className='img-fluid text-center w-100' alt=""></img>
                 <div className='text-center text-sm-start'>
                     <h2 className='heading'>Web & Mobile App Development</h2>
                     <p >Your web and mobile Apps are pieces of the puzzle to grow your business. We use frameworks which tailor content and engagement methods to respond to different intents shown by your potential customers who interact with your business online.</p>
@@ -19,7 +19,7 @@ const Content = () => {
                     <p >Your digital strategy should complement the overall marketing strategy of the company. In online marketing, each component will never work in isolation and every business needs a different mix. We provide a clear concept and strategic overview to find the most efficient model for your business.</p>
                     <CustomButton title={"LEARN MORE"} />
                 </div>
-                <img src={img1} className='img-fluid text-center w-100'></img>
+                <img src={img1} className='img-fluid text-center w-100' alt=""></img>
 
             </div>
         </div>
